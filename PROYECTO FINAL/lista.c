4e1733aacef72a69be0cf0a87c4f28f773aee49d
NaomiOrtiz26 <ortizvallado.naomi.a@gmail.com>
2026-05-30 23:36:04 -0600

    Update lista.c

--- PROYECTO FINAL/lista.c
+++ PROYECTO FINAL/lista.c
@@ -1,1 +1,31 @@
+#include <stdlib.h>
+#include "lista.h"
+
+void iniciarLista(Lista *l) {
+
+    l->inicio = NULL;
+}
+
+void insertarLista(Lista *l, void *dato) {
+
+    Nodo *nuevo = (Nodo*) malloc(sizeof(Nodo));
+
+    nuevo->dato = dato;
+    nuevo->sig = NULL;
+
+    if(l->inicio == NULL) {
+
+        l->inicio = nuevo;
+
+    } else {
+
+        Nodo *aux = l->inicio;
+
+        while(aux->sig != NULL) {
+            aux = aux->sig;
+        }
+
+        aux->sig = nuevo;
+    }
+}
 
