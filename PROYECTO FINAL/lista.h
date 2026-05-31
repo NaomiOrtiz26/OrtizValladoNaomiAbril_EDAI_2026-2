74fecd965365fbdc53aeb8d811c2906948330dfc
NaomiOrtiz26 <ortizvallado.naomi.a@gmail.com>
2026-05-30 23:35:27 -0600

    Update lista.h

--- PROYECTO FINAL/lista.h
+++ PROYECTO FINAL/lista.h
@@ -1,1 +1,19 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+typedef struct Nodo {
+    void *dato;
+    struct Nodo *sig;
+} Nodo;
+
+typedef struct {
+    Nodo *inicio;
+} Lista;
+
+void iniciarLista(Lista *l);
+
+void insertarLista(Lista *l, void *dato);
+
+#endif
+
 
