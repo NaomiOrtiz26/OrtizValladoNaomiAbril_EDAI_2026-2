8d575b310e9544a7566be7f399348f8baf9190b2
NaomiOrtiz26 <ortizvallado.naomi.a@gmail.com>
2026-05-30 23:34:33 -0600

    Update estructuras.h
    Define structures for Region, Zona, Regimiento, Batallon, Compania, and Soldado.

--- PROYECTO FINAL/estructuras.h
+++ PROYECTO FINAL/estructuras.h
@@ -1,1 +1,42 @@
+#ifndef ESTRUCTURAS_H
+#define ESTRUCTURAS_H
 
+typedef struct {
+    char id[11];
+    char nombre[41];
+} Region;
+
+typedef struct {
+    char id[11];
+    char nombre[41];
+    char idRegion[11];
+} Zona;
+
+typedef struct {
+    char id[11];
+    char nombre[41];
+    char tipo[20];
+    char idZona[11];
+} Regimiento;
+
+typedef struct {
+    char id[11];
+    int numero;
+    char tipo[20];
+    char idRegimiento[11];
+} Batallon;
+
+typedef struct {
+    char id[11];
+    char nombre[41];
+    char idBatallon[11];
+} Compania;
+
+typedef struct {
+    char id[11];
+    char nombre[41];
+    char grado[20];
+    char idCompania[11];
+} Soldado;
+
+#endif
