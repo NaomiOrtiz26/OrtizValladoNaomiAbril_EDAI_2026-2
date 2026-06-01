d3d26584fdc4acfda4b3eb1baad28216cb91f4b8
NaomiOrtiz26 <ortizvallado.naomi.a@gmail.com>
2026-05-31 22:39:52 -0600

    lista.h

--- PROYECTO FINAL/lista.h
+++ PROYECTO FINAL/lista.h
@@ -1,19 +1,28 @@
 #ifndef LISTA_H
 #define LISTA_H
+// Evita que este archivo se incluya más de una vez durante la compilación
 
 typedef struct Nodo {
     void *dato;
+    // Apuntador genérico que puede almacenar cualquier tipo de dato
+
     struct Nodo *sig;
+    // Apuntador al siguiente nodo de la lista enlazada
 } Nodo;
+// Definición de la estructura Nodo
 
 typedef struct {
     Nodo *inicio;
+    // Apuntador al primer nodo de la lista
 } Lista;
+// Definición de la estructura Lista
 
 void iniciarLista(Lista *l);
+// Declaración de la función que inicializa una lista vacía
 
 void insertarLista(Lista *l, void *dato);
+// Declaración de la función que inserta un dato en la lista
 
-#endif
+#endif // Fin de la directiva de inclusión única
 
 
